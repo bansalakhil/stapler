fda29ec2791d46ddaf648727ea7f2719938cb625
Iulian Stoian <iulian@webstarsltd.com>
2017-09-15 11:26:32 +0300

    Fixed the 504 error when trying to access the bucket from outside

--- src/Validator.php
+++ src/Validator.php
@@ -46,10 +46,17 @@ class Validator implements ValidatorInterface
             throw new Exceptions\InvalidUrlOptionException('Invalid Path: a bucket is required for s3 storage.', 1);
         }
 
-        $file = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/';
-        $file_headers = @get_headers($file);
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CONNECTTIMEOUT => 2,
+                CURLOPT_URL => 'http://169.254.169.254/latest/meta-data/iam/security-credentials/'
+        ]);
+        curl_exec($curl);
+        $response_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
 
-        if (!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+        if ($response_code == 0) {
             if (!$options['s3_client_config']['secret']) {
                 throw new Exceptions\InvalidUrlOptionException('Invalid Path: a secret is required for s3 storage.', 1);
             }
